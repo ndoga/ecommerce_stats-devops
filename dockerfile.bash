7a9e5d56a72f444ec39c98c3020fbf63dbbd25f9
Matteo Gandelli <111530378+ndoga@users.noreply.github.com>
2023-03-08 11:52:12 +0100

    Update dockerfile.bash

--- dockerfile.bash
+++ dockerfile.bash
@@ -1,7 +1,5 @@
-FROM ubuntu:latest
-
-RUN apt-get update && apt-get install -y apache2
-
-CMD ["apache2ctl", "-D", "FOREGROUND"]
-
+FROM ubuntu/apache2:latest
+ENV TZ=IT
+COPY index.html /var/www/html/
 EXPOSE 80
+CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
